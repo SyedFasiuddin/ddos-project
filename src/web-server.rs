277a18a1615802077a7f8d4bca8a620742fe03d2
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-04-04 15:24:49 +0530

    web: server static file

--- src/web-server.rs
+++ src/web-server.rs
@@ -1,12 +1,39 @@
+use std::borrow::Cow;
+
 use rocket::get;
+use rocket::http::ContentType;
+use rocket::http::Status;
 use rocket::routes;
+
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "public/"]
+struct Assets;
 
 #[get("/test")]
 fn hello() -> &'static str {
     "Hello World\n"
 }
 
+#[get("/")]
+fn index() -> (Status, (ContentType, Cow<'static, [u8]>)) {
+    match Assets::get("index.html") {
+        Some(embedded_file) => (Status::Ok, (ContentType::HTML, embedded_file.data)),
+        None => {
+            eprintln!("ERROR: unable to get `public/index.html`");
+            return (
+                Status::NotFound,
+                (ContentType::Plain, Cow::from("404".as_bytes())),
+            );
+        }
+    }
+}
+
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build().mount("/", routes![hello]).launch().await;
+    let _ = rocket::build()
+        .mount("/", routes![hello, index])
+        .launch()
+        .await;
 }
