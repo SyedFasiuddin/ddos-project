6b2d74c7bc8fbf660f8878c1f9a18c1acc188966
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-04-04 14:33:45 +0530

    web: rocket initial setup

--- src/web-server.rs
+++ src/web-server.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("web-server")
+use rocket::get;
+use rocket::routes;
+
+#[get("/test")]
+fn hello() -> &'static str {
+    "Hello World\n"
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build().mount("/", routes![hello]).launch().await;
 }
