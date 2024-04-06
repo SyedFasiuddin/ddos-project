0ba1a01bf00282d8acf33825d36b9117a41acee6
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-04-06 12:20:05 +0530

    handling /test route for simple web server

--- src/simple-web-server.rs
+++ src/simple-web-server.rs
@@ -1,24 +1,65 @@
-use std::io::BufRead;
 use std::io::BufReader;
+use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::process::exit;
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let peer_addr = match stream.peer_addr() {
+        Ok(addr) => {
+            eprintln!("INFO: got a connection from {addr}");
+            addr
+        }
+        Err(e) => {
+            eprintln!("INFO: got a connection, can't get address due to: {e}");
+            "0.0.0.0:0000"
+                .parse()
+                .expect("ERROR: client address not parsable")
+        }
+    };
 
-    let response = b"HTTP/1.1 200 OK\r\n\r\n";
-    stream.write_all(response).unwrap();
+    let mut buf_reader = BufReader::new(&mut stream);
+    let mut buffer = [0; 1024];
+
+    let mut response = String::new();
+    'outer: loop {
+        let bytes: Vec<_> = match buf_reader.read(&mut buffer) {
+            Ok(0) => {
+                eprintln!("INFO: {peer_addr} has closed the connection");
+                return;
+            }
+            Ok(n) => buffer[0..n].iter().cloned().collect(),
+            Err(e) => {
+                eprintln!("ERROR: cannot read from {peer_addr} due to: {e}");
+                return;
+            }
+        };
+
+        let http_request = String::from_utf8(bytes).expect("ERROR: {peer_addr} violates HTTP spec");
+        let http_request: Vec<_> = http_request.lines().collect();
+
+        for line in http_request {
+            if line == "GET /test HTTP/1.1" {
+                eprintln!("INFO: GET request for route /test from {peer_addr}");
+                response = "HTTP/1.1 200 OK\r\n\r\nHello World\n".to_string();
+            }
+
+            if line == "" {
+                // Got a complete HTTP request
+                break 'outer;
+            }
+        }
+    }
+
+    match stream.write_all(response.as_bytes()) {
+        Ok(()) => eprintln!("INFO: sent response to {peer_addr} successfully, closing connection"),
+        Err(e) => eprintln!("ERROR: sending response to {peer_addr} due to: {e}"),
+    }
 }
 
 fn main() {
-    let ip = "0.0.0.0";
+    let ip = "127.0.0.1";
     let port = "8000";
     let address = format!("{ip}:{port}");
 
@@ -35,13 +76,7 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                match stream.peer_addr() {
-                    Ok(addr) => eprintln!("INFO: got a connection from {addr}"),
-                    Err(e) => eprintln!("INFO: got a connection, can't get address due to: {e}"),
-                }
-                handle_connection(stream);
-            }
+            Ok(stream) => handle_connection(stream),
             Err(e) => {
                 eprintln!("ERROR: incoming connection resulted in error: {e}");
                 exit(1);
