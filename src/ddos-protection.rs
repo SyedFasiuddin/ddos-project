b19cfa49d9e53747aaad0f6f31455aa7d4908d07
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-04-09 16:30:08 +0530

    feat: ddos protection service as transparent proxy

--- src/ddos-protection.rs
+++ src/ddos-protection.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("ddos-protection")
+use std::path::PathBuf;
+
+use rocket::get;
+use rocket::http::ContentType;
+use rocket::routes;
+
+const SERVER_IP: &str = "127.0.0.1";
+const SERVER_PORT: &str = "8000";
+
+#[get("/test")]
+async fn test() -> (ContentType, Vec<u8>) {
+    let url = format!("http://{SERVER_IP}:{SERVER_PORT}/test");
+    match reqwest::get(url).await {
+        Ok(response) => {
+            let bytes = response.bytes().await.unwrap();
+            (ContentType::Plain, bytes.to_vec())
+        }
+        Err(e) => {
+            eprintln!("ERROR: something went wrong: {e}");
+            (ContentType::Plain, vec![])
+        }
+    }
+}
+
+#[get("/<file..>")]
+async fn dist(file: PathBuf) -> (ContentType, Vec<u8>) {
+    let url = format!("http://{SERVER_IP}:{SERVER_PORT}/{}", file.display());
+    match reqwest::get(url).await {
+        Ok(response) => {
+            let headers = response.headers();
+            let content_type = headers.get("Content-Type").unwrap().to_str().unwrap();
+            let content_type = ContentType::parse_flexible(content_type).unwrap();
+            let bytes = response.bytes().await.unwrap();
+
+            (content_type, bytes.to_vec())
+        }
+        Err(e) => {
+            eprintln!("ERROR: something went wrong: {e}");
+            (ContentType::Plain, vec![])
+        }
+    }
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", routes![test, dist])
+        .launch()
+        .await;
 }
