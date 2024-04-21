529daea2c7da8ef05744e1d285dc3d0f867a84dd
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-04-21 21:30:48 +0530

    fix: clippy

--- src/simple-web-server.rs
+++ src/simple-web-server.rs
@@ -11,15 +11,13 @@ use rust_embed::RustEmbed;
 struct Assets;
 
 fn route_test() -> Vec<u8> {
-    format!(
-        "\
-        HTTP/1.1 200 OK\r\n\
-        Content-Type: text/plain\r\n\
-        Content-Length: 12\r\n\
-        \r\n\
-        Hello World\n\
-        "
-    )
+    "\
+    HTTP/1.1 200 OK\r\n\
+    Content-Type: text/plain\r\n\
+    Content-Length: 12\r\n\
+    \r\n\
+    Hello World\n\
+    "
     .into()
 }
 
