a25b745f480a9ae1642b9f222277b43b5d5522c0
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-04-21 21:28:34 +0530

    feat: rate limiting

--- src/ddos-protection.rs
+++ src/ddos-protection.rs
@@ -1,14 +1,29 @@
+use std::env;
+use std::net::IpAddr;
 use std::path::PathBuf;
+use std::sync::Mutex;
+use std::time::{Duration, SystemTime};
 
 use rocket::get;
 use rocket::http::ContentType;
 use rocket::routes;
+use rocket::State;
 
 const SERVER_IP: &str = "127.0.0.1";
 const SERVER_PORT: &str = "8000";
 
+#[derive(Debug, Default)]
+struct ServerState {
+    req: Mutex<Vec<(IpAddr, SystemTime)>>,
+}
+
 #[get("/test")]
-async fn test() -> Option<(ContentType, Vec<u8>)> {
+async fn test(ip: IpAddr, state: &State<ServerState>) -> Option<(ContentType, Vec<u8>)> {
+    if should_block(ip, state) {
+        println!("ERROR: blocking ip: {ip}");
+        return None;
+    }
+
     let url = format!("http://{SERVER_IP}:{SERVER_PORT}/test");
     match reqwest::get(url).await {
         Ok(response) => {
@@ -23,7 +38,16 @@ async fn test() -> Option<(ContentType, Vec<u8>)> {
 }
 
 #[get("/<file..>")]
-async fn dist(file: PathBuf) -> Option<(ContentType, Vec<u8>)> {
+async fn dist(
+    file: PathBuf,
+    ip: IpAddr,
+    state: &State<ServerState>,
+) -> Option<(ContentType, Vec<u8>)> {
+    if should_block(ip, state) {
+        println!("ERROR: blocking ip: {ip}");
+        return None;
+    }
+
     let url = format!("http://{SERVER_IP}:{SERVER_PORT}/{}", file.display());
     match reqwest::get(url).await {
         Ok(response) => {
@@ -41,10 +65,33 @@ async fn dist(file: PathBuf) -> Option<(ContentType, Vec<u8>)> {
     }
 }
 
+fn should_block(ip: IpAddr, state: &State<ServerState>) -> bool {
+    let now = SystemTime::now();
+    let connections = &mut *state.req.lock().unwrap();
+    let time = {
+        let time = env::var("DDOS_TIMEOUT_DURATION").unwrap_or("60".to_string());
+        time.parse::<u64>().unwrap()
+    };
+    connections.retain(|c| now.duration_since(c.1).unwrap() < Duration::new(time, 0));
+    let c = connections.iter().filter(|&c| c.0 == ip).count();
+
+    let count = {
+        let count = env::var("DDOS_LIMIT_WITHIN_DURATION").unwrap_or("100".to_string());
+        count.parse::<usize>().unwrap()
+    };
+    if c >= count {
+        return true;
+    }
+    connections.push((ip, now));
+
+    false
+}
+
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
         .mount("/", routes![test, dist])
+        .manage(ServerState::default())
         .launch()
         .await;
 }
